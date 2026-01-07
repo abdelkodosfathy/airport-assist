8e7d6c93d795cb0379aec7e910e34793b9692371
abdelkodosfathy <abdelkodosfathy@gmail.com>
2026-01-07 23:53:13 +0200

    adding chauffuer services some forms

---
+++ app/chauffeur-services/billing-information/page.tsx
@@ -0,0 +1,143 @@
+// app/chauffeur-services/BillingInformationPage/page.tsx
+
+import { Button } from "@/components/ui/button";
+import Separator from "@/components/ui/formSeparator";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import car1 from "@/public/car1.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function BillingInformationPage() {
+  return (
+    <>
+      <div className="h-full w-full max-w-6/11 space-y-4">
+        <div className="shadow-md px-4.5 py-6 bg-white rounded-2xl w-full">
+          <div>
+            <h3 className="mb-2 font-semibold">Billing Information</h3>
+            <p className="text-[#6D6D6D]">
+              Main point of communication for this reservation
+            </p>
+          </div>
+          <Separator />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 w-full">
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="first-name">First Name</Label>
+              <Input
+                id="first-name"
+                placeholder="Enter First Name"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="last-name">Last Name</Label>
+              <Input
+                id="last-name"
+                placeholder="Enter Last Name"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="first-name">Email</Label>
+              <Input
+                id="email"
+                placeholder="Example@gamil.com"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="last-name">Phone</Label>
+              <Input
+                id="phone"
+                placeholder="0101 434 3413"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="shadow-md px-4.5 py-6 bg-white rounded-2xl w-full">
+          <div>
+            <h3 className="mb-2 font-semibold">Billing Address</h3>
+          </div>
+          <Separator />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 w-full">
+            <div className="space-y-2 col-span-2 mb-6">
+              <Label htmlFor="first-name">Address</Label>
+              <Input
+                id="address"
+                placeholder="Enter First Name"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="last-name">Town / City</Label>
+              <Input
+                id="town-city"
+                placeholder="City"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="first-name">Country</Label>
+              <Input
+                id="country"
+                placeholder="Country"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+            <div className="space-y-2 col-span-1 mb-6">
+              <Label htmlFor="last-name">Post Code</Label>
+              <Input
+                id="post-code"
+                placeholder="Enter The Post Code"
+                className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" w-5/11">
+        <div className="rounded-lg bg-white">
+          <h3>
+            Mercedes S-Class <span>( New Shape )</span>
+          </h3>
+          <div className="flex">
+            <div>
+              <p>Premium Sedan – Elegant comfort for up to 2 passengers.</p>
+              <Image src={car1} width={265} height={132} alt="car" />
+            </div>
+            <div>
+              <ul>
+                <li>2 passengers</li>
+                <li>2 luggage pieces</li>
+                <li>Luggage assist</li>
+                <li>Licensed chauffeurs</li>
+                <li>Complimentary water</li>
+                <li>Complimentary Wi-Fi</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <Button
+          asChild
+          variant="outline"
+          className="
+            col-span-2
+            cursor-pointer 
+            border-[#D1D1D1] 
+            text-[#7A7A7A] 
+            bg-[#ECECEC]
+            hover:border-[#664F31]  
+            hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] 
+            hover:text-white 
+            duration-0
+          "
+        >
+          <Link href={"/chauffeur-services/billing-information/checkout"}>Continue</Link>
+        </Button>
+      </div>
+    </>
+  );
+}
--- app/chauffeur-services/components/Pickup.tsx
+++ app/chauffeur-services/components/Pickup.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Minus, Plus } from "lucide-react";
@@ -9,162 +11,62 @@ import CarriedBag from "@/components/custom icons/carriedBag";
 import MeetAndGreet from "@/components/custom icons/meetAndGreet";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import clsx from "clsx";
+import Separator from "@/components/ui/formSeparator";
+import Link from "next/link";
 
+type ServiceType = "one-way" | "round-trip" | "hourly";
 type Props = {};
 
 const Pickup = (props: Props) => {
+  const [selectedService, setSelectedService] =
+    useState<ServiceType>("one-way");
+
   return (
     <div className="flex-2 h-full">
       <div className="flex gap-3.5 mb-4">
         <Button
+        onClick={() => setSelectedService("one-way")}
           variant="outline"
-          className="
-            mt-6
-            w-max 
-            cursor-pointer 
-            border-[#D1D1D1] 
-            text-[#7A7A7A] 
-            bg-[#ECECEC]
-            hover:border-[#664F31]  
-            hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] 
-            hover:text-white 
-            duration-0
-          "
+          className={clsx(
+            " mt-6 w-max  cursor-pointer  border-[#D1D1D1]  text-[#7A7A7A]  bg-[#ECECEC] hover:border-[#664F31]   hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)]  hover:text-white  duration-0",
+            selectedService === "one-way"
+              ? "border-[#664F31] bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] text-white"
+              : ""
+          )}
         >
           On a way
         </Button>
         <Button
+        onClick={() => setSelectedService("round-trip")}
           variant="outline"
-          className="
-            mt-6
-            w-max 
-            cursor-pointer 
-            border-[#D1D1D1] 
-            text-[#7A7A7A] 
-            bg-[#ECECEC]
-            hover:border-[#664F31]  
-            hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] 
-            hover:text-white 
-            duration-0
-          "
+          className={clsx(
+            " mt-6 w-max  cursor-pointer  border-[#D1D1D1]  text-[#7A7A7A]  bg-[#ECECEC] hover:border-[#664F31]   hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)]  hover:text-white  duration-0",
+            selectedService === "round-trip"
+              ? "border-[#664F31] bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] text-white"
+              : ""
+          )}
         >
           Round Trip
         </Button>
         <Button
+        onClick={() => setSelectedService("hourly")}
           variant="outline"
-          className="
-            mt-6
-            w-max 
-            cursor-pointer 
-            border-[#D1D1D1] 
-            text-[#7A7A7A] 
-            bg-[#ECECEC]
-            hover:border-[#664F31]  
-            hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] 
-            hover:text-white 
-            duration-0
-          "
+          className={clsx(
+            " mt-6 w-max  cursor-pointer  border-[#D1D1D1]  text-[#7A7A7A]  bg-[#ECECEC] hover:border-[#664F31]   hover:bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)]  hover:text-white  duration-0",
+            selectedService === "hourly"
+              ? "border-[#664F31] bg-[linear-gradient(179.26deg,#664F31_0.64%,#DFB08D_223.79%)] text-white"
+              : ""
+          )}
         >
           Hourly
         </Button>
       </div>
-      <div className="flex items-center mb-4 gap-4 rounded-lg bg-[#FFFBEF] px-4 py-3 border border-[#7B5A414D] text-[#7B5A41]">
-        <div className="min-w-6 w-6 min-h-6 h-6 text-lg bg-[#7B5A41] rounded-full grid place-content-center">
-          <p className="text-white">!</p>
-        </div>
-        <p>
-          One-way is a professional chauffeur service from point A to point B.
-        </p>
-      </div>
-
+      <InnerToast text="One-way is a professional chauffeur service from point A to point B." />
       <div className="flex gap-4 ">
-        <div className="shadow-lg px-4.5 py-6 bg-white rounded-2xl h-full w-full max-w-6/11">
-          <div>
-            <h3 className="mb-2">Pickup</h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6 w-full">
-              <div className="space-y-2 col-span-2">
-                {/* <Label htmlFor="from">From</Label> */}
-                <Input
-                  id="from"
-                  placeholder="First Name"
-                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-                />
-              </div>
-              <div className="space-y-2 col-span-2">
-                {/* <Label htmlFor="drop-off">Drop Off</Label> */}
-                <Input
-                  id="drop-off"
-                  placeholder="First Name"
-                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-                />
-              </div>
-              <div className="space-y-2 col-span-1">
-                {/* <Label htmlFor="pickup-date">Pickup date</Label> */}
-                <Input
-                  id="pickup-date"
-                  placeholder="First Name"
-                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-                />
-              </div>
-              <div className="space-y-2 col-span-1">
-                {/* <Label htmlFor="pickup-time">Pickup time</Label> */}
-                <Input
-                  id="pickup-time"
-                  placeholder="First Name"
-                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-                />
-              </div>
-            </div>
-          </div>
+        <PickupForm serviceType={selectedService} />
 
-          <Separator />
-          <div>
-            <h3 className="mb-2">Flight</h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6 w-full">
-              <div className="space-y-2 col-span-1">
-                {/* <Label htmlFor="flight">Flight</Label> */}
-                <Input
-                  id="flight"
-                  placeholder="Air Lines"
-                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-                />
-              </div>
-              <div className="space-y-2 col-span-1">
-                {/* <Label htmlFor="flight">Flight</Label> */}
-                <Input
-                  id="flight"
-                  placeholder="Air Lines"
-                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-                />
-              </div>
-            </div>
-          </div>
-          <Separator />
-          <div className="flex gap-16 mt-3">
-            <NumperInput title="Passengers" />
-            <NumperInput title="Luggage" />
-          </div>
-          <Separator />
-          <div>
-            <div className="flex justify-between p-4 items-center bg-[#F2F3F5] rounded-md my-4">
-              <p className="flex gap-2 text-lg items-center">
-                <MeetAndGreet />
-                Airport Meet & greet service
-              </p>
-              <Switch className="scale-110 data-[state=checked]:bg-[#7B5A41]" />
-            </div>
-            <h3 className="font-semibold mb-2">
-              Additional requirements{" "}
-              <span className="text-sm font-light text-[#7A7A7A]">
-                (optional)
-              </span>
-            </h3>
-            <Textarea
-              placeholder="Any Special Notes"
-              className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
-            />
-          </div>
-        </div>
         <div className="grid grid-cols-2 gap-3 w-5/11">
           <CarCard
             title="Mercedes V-Class"
@@ -187,8 +89,19 @@ const Pickup = (props: Props) => {
             desc="Premium SUV – Elegant comfort for up to 4 passengers."
             image={car2}
           />
+          <CarCard
+            title="Range Rover Vogue"
+            desc="Premium SUV – Elegant comfort for up to 4 passengers."
+            image={car2}
+          />
+          <CarCard
+            title="Range Rover Vogue"
+            desc="Premium SUV – Elegant comfort for up to 4 passengers."
+            image={car2}
+          />
 
           <Button
+          asChild
             variant="outline"
             className="
             col-span-2
@@ -202,7 +115,9 @@ const Pickup = (props: Props) => {
             duration-0
           "
           >
+            <Link href={"/chauffeur-services/billing-information"}>
             Continue
+            </Link>
           </Button>
         </div>
       </div>
@@ -212,10 +127,182 @@ const Pickup = (props: Props) => {
 
 export default Pickup;
 
-const Separator = () => (
-  <span className="inline-block w-full h-0.5 bg-[#E5E5E5]"></span>
-);
-const NumperInput = ({ title }: { title: string }) => (
+const PickupForm = ({ serviceType }: { serviceType?: ServiceType }) => {
+  return (
+    <div className="shadow-md px-4.5 py-6 bg-white rounded-2xl h-full w-full max-w-6/11">
+      <div className="my-2">
+        <h3 className="mb-2 font-semibold">Pickup</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 w-full">
+          <div className="space-y-2 col-span-2 mb-6">
+            {/* <Label htmlFor="from">From</Label> */}
+            <Input
+              id="from"
+              placeholder="From"
+              className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+            />
+          </div>
+          <div className="space-y-2 col-span-2">
+            {/* <Label htmlFor="drop-off">Drop Off</Label> */}
+            <Input
+              id="drop-off"
+              placeholder="Drop Off"
+              className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+            />
+          </div>
+
+          <div className="col-span-2 flex justify-end my-2">
+            <Button
+              variant={"ghost"}
+              className="cursor-pointer align-end p-0 hover:bg-white hover:text-[#614631] text-end font-semibold text-[#7B5A41]"
+            >
+              + Add stop
+            </Button>
+          </div>
+          <div className="space-y-2 col-span-1">
+            {/* <Label htmlFor="pickup-date">Pickup date</Label> */}
+            <Input
+              id="pickup-date"
+              placeholder="Pickup data"
+              className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+            />
+          </div>
+          <div className="space-y-2 col-span-1">
+            {/* <Label htmlFor="pickup-time">Pickup time</Label> */}
+            <Input
+              id="pickup-time"
+              placeholder="Pickup time"
+              className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+            />
+          </div>
+        </div>
+      </div>
+
+      <Separator />
+
+      {/* return */}
+      {serviceType === "round-trip" && (
+        <>
+          <div className="my-2">
+            <h3 className="mb-2 font-semibold">Return</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 w-full">
+              <div className="space-y-2 col-span-2 mb-4">
+                {/* <Label htmlFor="from">From</Label> */}
+                <Input
+                  id="from"
+                  placeholder="Return From"
+                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+                />
+              </div>
+              <div className="space-y-2 col-span-2">
+                {/* <Label htmlFor="drop-off">Drop Off</Label> */}
+                <Input
+                  id="drop-off"
+                  placeholder="Return Drop Off"
+                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+                />
+              </div>
+
+              <div className="col-span-2 flex justify-end my-2">
+                <Button
+                  variant={"ghost"}
+                  className="cursor-pointer p-0 hover:bg-white hover:text-[#7B5A41] text-end font-semibold text-[#7B5A41]"
+                >
+                  + Add stop
+                </Button>
+              </div>
+              <div className="space-y-2 col-span-1">
+                {/* <Label htmlFor="pickup-date">Pickup date</Label> */}
+                <Input
+                  id="pickup-date"
+                  placeholder="Return Pickup data"
+                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+                />
+              </div>
+              <div className="space-y-2 col-span-1">
+                {/* <Label htmlFor="pickup-time">Pickup time</Label> */}
+                <Input
+                  id="pickup-time"
+                  placeholder="Return Pickup time"
+                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+                />
+              </div>
+            </div>
+          </div>
+          <Separator />
+        </>
+      )}
+
+      {/* flight */}
+
+      {(serviceType === "round-trip" || serviceType === "one-way") && (
+        <>
+          <div className="my-2">
+            <h3 className="mb-2 font-semibold">Flight</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6 w-full">
+              <div className="space-y-2 col-span-1">
+                {/* <Label htmlFor="flight">Flight</Label> */}
+                <Input
+                  id="flight"
+                  placeholder="Air Lines"
+                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+                />
+              </div>
+              <div className="space-y-2 col-span-1">
+                {/* <Label htmlFor="flight">Flight</Label> */}
+                <Input
+                  id="flight"
+                  placeholder="Flight Number"
+                  className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+                />
+              </div>
+            </div>
+          </div>
+          <Separator />
+        </>
+      )}
+
+      {serviceType === "hourly" && (
+        <>
+          <div className="mb-2 mt-4">
+            <div className="flex justify-between">
+              <h3 className="mb-2 font-semibold">Hours</h3>
+              <NumberInput title="" />
+            </div>
+            <div className="my-2">
+              <InnerToast text="with 40 miles included in the booking price. Any additional mileage will be charged at £4 per mile after the service." />
+            </div>
+          </div>
+          <Separator />
+        </>
+      )}
+      <div className="flex gap-16 mt-3">
+        <NumberInput title="Passengers" />
+        <NumberInput title="Luggage" />
+      </div>
+      <Separator/>
+      <div>
+        <div className="flex justify-between p-4 items-center bg-[#F2F3F5] rounded-md my-4">
+          <p className="flex gap-2 text-lg items-center">
+            <MeetAndGreet />
+            Airport Meet & greet service
+          </p>
+          <Switch className="scale-110 data-[state=checked]:bg-[#7B5A41]" />
+        </div>
+        <h3 className="font-semibold mb-2">
+          Additional requirements{" "}
+          <span className="text-sm font-light text-[#7A7A7A]">(optional)</span>
+        </h3>
+        <Textarea
+          placeholder="Any Special Notes"
+          className="pl-4 pr-10 bg-[#F4F4F4] border border-[#E0E0E0]"
+        />
+      </div>
+    </div>
+  );
+};
+
+
+const NumberInput = ({ title }: { title: string }) => (
   <div className="flex flex-1 justify-between items-center">
     <p className="font-semibold">{title}</p>
     <div className="flex gap-2 items-center">
@@ -303,3 +390,12 @@ function Radio({ selected }: RadioProps) {
     </div>
   );
 }
+
+const InnerToast = ({ text }: { text: string }) => (
+  <div className="flex items-center mb-4 gap-4 rounded-lg bg-[#FFFBEF] px-4 py-3 border border-[#7B5A414D] text-[#7B5A41]">
+    <div className="min-w-6 w-6 min-h-6 h-6 text-lg bg-[#7B5A41] rounded-full grid place-content-center">
+      <p className="text-white">!</p>
+    </div>
+    <p>{text}</p>
+  </div>
+);
